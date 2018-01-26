ebbfeb7a5003848c3921153c0efb75bbb2b45cbd
Carlo Manalese <CarloManalese@carlos-mbp.attlocal.net>
2018-01-25 23:04:52 -0800

    Almost Motorcycle Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,26 @@
 
-
-
 var total=0;
-
 var number;
 var genNum;
+
 function targetNumber () { 
 	genNum = Math.floor(Math.random()*102 +19);
-	
-	
-}
 
-	
+}//targetNumber end
 
-targetNumber();
+ targetNumber();
 
 function crystalNumber(){
 	 return Math.floor(Math.random()*12 +1);
 	
+}//crystalNumber end
 
-}
 var gem1=crystalNumber();
 var gem2=crystalNumber();
 var gem3=crystalNumber();
 var gem4=crystalNumber();
 
 $(document).ready(function(){
-
-
 
 $("#computer").text(genNum);
 
@@ -52,5 +45,24 @@ $("#four").on("click",function(){
 	$("#total1").text(total);
 });
 
+var points=0;
 
-});
+function up(){
+	if(total === targetNumber){
+		points = points +1;
+		$("#win").text(points)
+
+	}
+	else if(total>targetNumber){
+		lose
+	}
+}
+
+
+
+
+
+
+
+
+});//document.text end
