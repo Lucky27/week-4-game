3ce386062754021e36f361718ab42df0ff689227
Carlo Manalese <CarloManalese@carlos-mbp.attlocal.net>
2018-01-25 19:12:36 -0800

    Crystal Collector Game

---
+++ assets/javascript/game.js
@@ -0,0 +1,56 @@
+
+
+
+var total=0;
+
+var number;
+var genNum;
+function targetNumber () { 
+	genNum = Math.floor(Math.random()*102 +19);
+	
+	
+}
+
+	
+
+targetNumber();
+
+function crystalNumber(){
+	 return Math.floor(Math.random()*12 +1);
+	
+
+}
+var gem1=crystalNumber();
+var gem2=crystalNumber();
+var gem3=crystalNumber();
+var gem4=crystalNumber();
+
+$(document).ready(function(){
+
+
+
+$("#computer").text(genNum);
+
+$("#one").on("click",function(){
+	total=gem1 +total;
+	$("#total1").text(total);
+	console.log(total);
+});
+
+$("#two").on("click",function(){
+	total=gem2 +total;
+	$("#total1").text(total);
+});
+
+$("#three").on("click",function(){
+	total=gem3 +total;
+	$("#total1").text(total);
+});
+
+$("#four").on("click",function(){
+	total=gem4 +total;
+	$("#total1").text(total);
+});
+
+
+});
